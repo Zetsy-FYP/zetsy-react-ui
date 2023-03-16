fad4786b70d9522a95aeaf865aa241324ac8fa43
rpc404 <theboringschool.org@gmail.com>
2023-03-16 15:10:07 +0545

    formatting codebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import App from "./App";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "remixicon/fonts/remixicon.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </>
+);

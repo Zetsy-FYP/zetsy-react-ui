6c2e088feff6ee62ebeb96cbdc8cbae8b77567e4
rpc404 <theboringschool.org@gmail.com>
2023-03-17 17:10:54 +0545

    feat:navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,19 +1,15 @@
 import { Button } from "@mui/material";
 import React from "react";
-import { motion, useTransform } from "framer-motion";
 
 import "./styles/style.css";
 
 export default function NavbarHome() {
+  // Logo Dynamic Animation
   const [scrollYLogoTransform, setScrollYLogoTransform] = React.useState(0);
   const [scrollYLogoMargin, setScrollYLogoMargin] = React.useState(0);
 
-  console.log(scrollYLogoTransform);
-
   window.addEventListener("scroll", () => {
-    setScrollYLogoTransform(
-      window.scrollY >= 148.88 ? 148.88 : window.scrollY
-    );
+    setScrollYLogoTransform(window.scrollY >= 148.88 ? 148.88 : window.scrollY);
     setScrollYLogoMargin(window.scrollY);
   });
 
@@ -21,6 +17,19 @@ export default function NavbarHome() {
     fontSize: `${8 - (scrollYLogoTransform * 2.5) / 60}rem`,
     top: `${scrollYLogoMargin >= 140 ? 80 : 80 - (scrollYLogoMargin - 140)}px`,
   };
+
+  const [isFocused, setIsFocused] = React.useState(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  // search input validation
+  const [inputValue, setInputValue] = React.useState("");
 
   return (
     <div className="navbarHome__container">
@@ -47,7 +56,7 @@ export default function NavbarHome() {
             <i className="ri-search-2-line"></i>
           </Button>
 
-          <input type="text" />
+          <input type="text" onChange={e => setInputValue(e.target.value)} onFocus={handleFocus} onBlur={handleBlur} />
 
           <Button className="borderRight">
             <i className="ri-map-pin-line"></i>
@@ -56,9 +65,11 @@ export default function NavbarHome() {
             <i className="ri-shopping-cart-line"></i>
           </Button>
         </div>
-        <div className="menu__logo">
-          <motion.h1 style={initialLogoStyle}>Zetsy.</motion.h1>
-        </div>
+        {!isFocused && window.scrollY !== 148.88 && inputValue === "" && (
+          <div className="menu__logo">
+            <h1 style={initialLogoStyle}>Zetsy.</h1>
+          </div>
+        )}
       </div>
       <div className="menuHighlights right">
         <div>

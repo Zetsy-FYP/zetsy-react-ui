9d4c3a4740e62ba49b310ea767a747337e8ef11a
rpc404 <theboringschool.org@gmail.com>
2023-03-27 03:46:15 +0545

    feat:added about us

---
+++ src/pages/about.jsx
@@ -0,0 +1,181 @@
+import React from "react";
+import styles from "@/styles/about.module.css";
+
+export default function About() {
+  return (
+    <div className="aboutUs__container">
+      <div className="landingBanner">
+        <div>A believer</div>
+        <div>
+          <p>world</p>
+          <p>
+            A creative journey
+            <br /> that changes the
+            <br /> way we look at our
+            <br /> daily essentials.
+          </p>
+        </div>
+        <div>starts in</div>
+        <div>your phone.</div>
+      </div>
+
+      <div className="zetsyIntroduction">
+        <h1>
+          "Since the start of the
+          <br /> Zetsy journey, we aim to continue
+          <br /> to merge our backgrounds in
+          <br /> technology and integrations
+          <br /> to create web to human
+          <br />
+          essentials, right here in the Nepal."
+        </h1>
+        <div>
+          <h1>How it all started</h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+            officiis sapiente corrupti dolorum quibusdam consequatur cumque,
+            voluptatibus soluta vel explicabo sit provident beatae veritatis
+            temporibus voluptatem dolore quam consequuntur. Tenetur!
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+            officiis sapiente corrupti dolorum quibusdam consequatur cumque,
+            voluptatibus soluta vel explicabo sit provident beatae veritatis
+            temporibus voluptatem dolore quam consequuntur. Tenetur!
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+            officiis sapiente corrupti dolorum quibusdam consequatur cumque,
+            voluptatibus soluta vel explicabo sit provident beatae veritatis
+            temporibus voluptatem dolore quam consequuntur. Tenetur!
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+            officiis sapiente corrupti dolorum quibusdam consequatur cumque,
+            voluptatibus soluta vel explicabo sit provident beatae veritatis
+            temporibus voluptatem dolore quam consequuntur. Tenetur!
+            <br />
+            <br />
+          </p>
+        </div>
+      </div>
+
+      <div className="zetsyImages">
+        <img
+          src="https://images.unsplash.com/photo-1535957998253-26ae1ef29506?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80"
+          loading="lazy"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1542546068979-b6affb46ea8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+          loading="lazy"
+          alt=""
+        />
+      </div>
+
+      <div className="zetsyCommunity">
+        <h1>
+          A community
+          <br />
+          build on CREATIVITY
+          <br />
+          driven by VALUES.
+        </h1>
+        <h1 className="divider">*</h1>
+        <div className="community">
+          <div>
+            <img
+              src="https://ik.imagekit.io/jckalabs/Zetsy/327268592_853679075707120_7819938780839728950_n.jpg?updatedAt=1679246534521"
+              loading="lazy"
+              alt=""
+            />
+            <p>Sudeep Mishra</p>
+            <span>Platform Wizard</span>
+            <img
+              src="https://ik.imagekit.io/jckalabs/Zetsy/57538363.jpeg?updatedAt=1679246673878"
+              loading="lazy"
+              alt=""
+            />
+            <p>Suraj Gaire</p>
+            <span>Theme Wizard</span>
+          </div>
+          <div>
+            <img
+              src="https://ik.imagekit.io/jckalabs/Zetsy/1517277165000.jpeg?updatedAt=1679246736576"
+              loading="lazy"
+              alt=""
+            />
+            <p>Nirdesh Acharya Dixit</p>
+            <span>Growth Hacker</span>
+            <img
+              src="https://ik.imagekit.io/jckalabs/Zetsy/Screenshot_from_2023-03-19_23-46-12.png?updatedAt=1679248895986"
+              loading="lazy"
+              alt=""
+            />
+            <p>Jessica Singh Shresth</p>
+            <span>Finance Manager</span>
+            <img
+              src="https://ik.imagekit.io/jckalabs/Zetsy/Screenshot_from_2023-03-22_22-01-54.png?updatedAt=1679501928957"
+              loading="lazy"
+              alt=""
+            />
+            <p>Bibek Angdembe</p>
+            <span>Frontend Dev</span>
+          </div>
+          <div>
+            <h2>We care about our people</h2>
+            <div>
+              <img
+                src="https://ik.imagekit.io/jckalabs/Zetsy/Screenshot_from_2023-03-19_23-05-03.png?updatedAt=1679246413143"
+                loading="lazy"
+                alt=""
+              />
+              <p>Laxman Rai</p>
+              <span>Web3 Wizard</span>
+            </div>
+          </div>
+          <div>
+            <img
+              src="https://ik.imagekit.io/jckalabs/Zetsy/Screenshot_from_2023-03-21_00-32-41.png?updatedAt=1679338073104"
+              loading="lazy"
+              alt=""
+            />
+            <p>Shishir Shrestha</p>
+            <span>PR Manager</span>
+            <img
+              src="https://ik.imagekit.io/jckalabs/Zetsy/Screenshot_from_2023-03-21_00-31-19.png?updatedAt=1679338000530"
+              loading="lazy"
+              alt=""
+            />
+            <p>Anmol Bhattarai</p>
+            <span>Finance Manager</span>
+            <img
+              src="https://ik.imagekit.io/jckalabs/Zetsy/Screenshot_from_2023-03-21_00-34-35.png?updatedAt=1679338187686"
+              loading="lazy"
+              alt=""
+            />
+            <p>Hritik Bhandari</p>
+            <span>Human Resource</span>
+          </div>
+          <div>
+            <img
+              src="https://ik.imagekit.io/jckalabs/Zetsy/Screenshot_from_2023-03-21_00-37-49.png?updatedAt=1679338382169"
+              loading="lazy"
+              alt=""
+            />
+            <p>Sarbendra Sigdel</p>
+            <span>Marketing Guru</span>
+            <img
+              src="https://ik.imagekit.io/jckalabs/Zetsy/Screenshot_from_2023-03-21_00-39-22.png?updatedAt=1679338474403"
+              loading="lazy"
+              alt=""
+            />
+            <p>Reewaz Neupane</p>
+            <span>Backend Dev</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

b5db434df9bd7f292a039bd834f3e25d66e26f95
rpc404 <theboringschool.org@gmail.com>
2023-03-26 19:46:31 +0545

    Updated Forgot - Responsive

--- src/pages/Auth/ForgotPassword/index.jsx
+++ src/pages/Auth/ForgotPassword/index.jsx
@@ -1,9 +1,16 @@
 import { Button } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import "./styles/style.css";
 
 export default function ForgotPassword() {
+  const navigation = useNavigate()
+
+  const handleNavigation = () => {
+    window.scrollTo(0,0);
+    navigation("/auth");
+  };
   return (
     <div className="forgotPassword__container">
       <form action="">
@@ -14,7 +21,7 @@ export default function ForgotPassword() {
         <Button className="authForm__button">Continue</Button>
         <div className="utilities mg-bttm-forgot">
           <p>Already have an account?</p>
-          <p onClick={() => handleSetFormType("login")}>Login</p>
+          <p onClick={() => handleNavigation()}>Login</p>
         </div>
       </form>
     </div>

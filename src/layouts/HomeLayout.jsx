d33370d3e04f92d932eac9133e333e3c3f977662
sudeepmi <sudeepm0599@gmail.com>
2023-03-30 02:09:54 +0545

    SEO

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -5,17 +5,19 @@ import React from "react";
 export default function HomeLayout({ children }) {
   const [windowWidth, setWindowWidth] = React.useState(0);
 
-  React.useLayoutEffect(() => {
-    function handleResize() {
-      setWindowWidth(window.innerWidth);
-    }
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  if(process.browser){
+    React.useLayoutEffect(() => {
+      function handleResize() {
+        setWindowWidth(window.innerWidth);
+      }
+  
+      handleResize();
+      window.addEventListener("resize", handleResize);
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }, []);
+  }
 
   return (
     <div>

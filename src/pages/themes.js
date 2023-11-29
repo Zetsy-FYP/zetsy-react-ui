ac737031a5caef3abddf449617a77cb4ca821f7a
13x54n <13x54r@gmail.com>
2023-11-29 09:09:25 -0500

    Added filter on theme page

--- src/pages/themes.js
+++ src/pages/themes.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ThemeCard from "@/components/ThemeCard";
 import styles from "@/styles/themes.module.css";
 import HomeLayout from "@/layouts/HomeLayout";
-import ThemesMock from "@/mocks/Themes.json"
+import ThemesMock from "@/mocks/Themes.json";
 
 export default function Themes() {
+  const [searchWord, setSearchWord] = useState("");
+  const [filteredList, setFilteredLists] = useState(ThemesMock);
+
+  const handleFilteredList = (e) => {
+    setSearchWord(e);
+    const filteredItems = ThemesMock.filter((theme) =>
+      theme.themeTitle.toLowerCase().includes(e.toLowerCase())
+    );
+    setFilteredLists(filteredItems);
+  };
   return (
     <HomeLayout>
       <div className={styles.themes__container}>
@@ -13,10 +23,12 @@ export default function Themes() {
           <input
             type="text"
             placeholder="Search Essence, Categories, Ingrediants for your awesome Brand..."
+            value={searchWord}
+            onChange={(e) => handleFilteredList(e.target.value)}
           />
         </div>
         <div className={styles.themesItems}>
-          {ThemesMock.map((item, i) => {
+          {filteredList.map((item, i) => {
             return <ThemeCard item={item} key={i} />;
           })}
         </div>
